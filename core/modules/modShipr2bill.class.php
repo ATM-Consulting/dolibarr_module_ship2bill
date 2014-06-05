2ae894e0cc4f51d1d140e653b26a425fd9725f6a
Gauthier <gauthier.verdol@atm-consulting.fr>
2014-06-05 12:02:55 +0200

    Numero 104140 pour shipr2bill

--- core/modules/modShipr2bill.class.php
+++ core/modules/modShipr2bill.class.php
@@ -45,7 +45,7 @@ class modShipr2bill extends DolibarrModules
         // Id for module (must be unique).
         // Use a free id here
         // (See in Home -> System information -> Dolibarr for list of used modules id).
-        $this->numero = 1556502; // 104000 to 104999 for ATM CONSULTING
+        $this->numero = 104140; // 104000 to 104999 for ATM CONSULTING
         // Key text used to identify module (for permissions, menus, etc...)
         $this->rights_class = 'mymodule';
 
