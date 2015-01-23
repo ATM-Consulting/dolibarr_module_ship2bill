9fb220b0e24db7f2a0139063e637d896bccd4c6a
Maxime Kohlhaas <maxime@atm-consulting.fr>
2015-01-23 10:18:25 +0100

    Ajout d'un droit pour premettre le téléchargement du PDF généré

--- core/modules/modShip2bill.class.php
+++ core/modules/modShip2bill.class.php
@@ -166,6 +166,11 @@ class modShip2bill extends DolibarrModules
 
 		// Permissions
 		$this->rights = array();		// Permission array used by this module
+		$this->rights[$r][0] = $this->numero + $r;	// Permission id (must not be already used)
+		$this->rights[$r][1] = 'Lire les expeditions facturables';	// Permission label
+		$this->rights[$r][3] = 1; 					// Permission by default for new user (0/1)
+		$this->rights[$r][4] = 'read';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
+		$r++;
 		$r=0;
 
 
