8f9190dea930384cf6ae8bd3e8df1d34089ccea7
Maxime Kohlhaas <maxime@atm-consulting.fr>
2016-07-05 10:15:42 +0200

    Option for checked by default

--- admin/ship2bill_setup.php
+++ admin/ship2bill_setup.php
@@ -246,6 +246,19 @@ print '<input type="submit" class="button" value="'.$langs->trans("Modify").'">'
 print '</form>';
 print '</td></tr>';
 
+$var=!$var;
+print '<tr '.$bc[$var].'>';
+print '<td>'.$langs->trans("CheckedByDefault").'</td>';
+print '<td align="center" width="20">&nbsp;</td>';
+print '<td align="right" width="300">';
+print '<form method="POST" action="'.$_SERVER['PHP_SELF'].'">';
+print '<input type="hidden" name="token" value="'.$_SESSION['newtoken'].'">';
+print '<input type="hidden" name="action" value="set_SHIP2BILL_CHECKED_BY_DEFAULT">';
+print $form->selectyesno("SHIP2BILL_CHECKED_BY_DEFAULT",$conf->global->SHIP2BILL_CHECKED_BY_DEFAULT,1);
+print '<input type="submit" class="button" value="'.$langs->trans("Modify").'">';
+print '</form>';
+print '</td></tr>';
+
 print '</table>';
 
 // Footer
