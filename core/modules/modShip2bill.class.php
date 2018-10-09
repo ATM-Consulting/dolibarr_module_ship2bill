b4a5a70cf14e4592246f7e3e8a7eb6ffd7458a5b
atm-quentin <quentin.vial-gouteyron@atm-consulting.fr>
2018-10-09 17:07:51 +0200

    new version

--- core/modules/modShip2bill.class.php
+++ core/modules/modShip2bill.class.php
@@ -55,7 +55,7 @@ class modShip2bill extends DolibarrModules
 		// Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
 		$this->description = "Module permettant de regrouper plusieurs expéditions en une seule facture";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
-		$this->version = '1.3.2';
+		$this->version = '1.4.0';
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
 		$this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
 		// Where to store the module in setup page (0=common,1=interface,2=others,3=very specific)
