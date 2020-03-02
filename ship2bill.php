38a66bfd222cd48c05ffd6a64d649d8beaff9aea
atm-ph <phf@atm-consulting.fr>
2020-03-02 10:15:02 +0100

    Fix tk11016 - si une expédition n'a pas de BL, alors dans la liste des exp facturable le champ Bon de réception affiche la valeur de la ligne précédante

--- ship2bill.php
+++ ship2bill.php
@@ -437,6 +437,7 @@ if ($resql)
 		if($conf->livraison_bon->enabled) {
             $shipment->fetchObjectLinked($shipment->id,$shipment->element);
             if(!empty($shipment->linkedObjects['delivery'])) $receiving=(! empty($shipment->linkedObjects['delivery'][key($shipment->linkedObjects['delivery'])])?$shipment->linkedObjects['delivery'][key($shipment->linkedObjects['delivery'])]:'');
+            else $receiving = '';
 
 			// Ref
 			print '<td>';
