124c50d91453f90df72561275e5e398a3bcf075b
John BOTELLA <john.botella@atm-consulting.fr>
2022-10-17 11:43:07 +0200

    Fix icon

--- ChangeLog.md
+++ ChangeLog.md
@@ -5,6 +5,7 @@ All notable changes to this project will be documented in this file.
 
 ## 1.8
 
+- FIX : Missing module icon  *17/10/2022* 1.8.1
 - NEW : Ajout de la class TechATM pour l'affichage de la page "A propos" *11/05/2022* 1.8.0
 
 ## 1.7
--- core/modules/modShip2bill.class.php
+++ core/modules/modShip2bill.class.php
@@ -58,7 +58,7 @@ class modShip2bill extends DolibarrModules
 		$this->editor_url = 'https://www.atm-consulting.fr';
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
 
-		$this->version = '1.8.0';
+		$this->version = '1.8.1';
 		// Url to the file with your last numberversion of this module
 		require_once __DIR__ . '/../../class/techatm.class.php';
 		$this->url_last_version = \ship2bill\TechATM::getLastModuleVersionUrl($this);
@@ -70,7 +70,7 @@ class modShip2bill extends DolibarrModules
 		// Name of image file used for this module.
 		// If file is in theme/yourtheme/img directory under name object_pictovalue.png, use this->picto='pictovalue'
 		// If file is in module/img directory under name object_pictovalue.png, use this->picto='pictovalue@module'
-		$this->picto='ship2bill@ship2bill';
+		$this->picto='module.svg@ship2bill';
 
 		// Defined all module parts (triggers, login, substitutions, menus, css, etc...)
 		// for default path (eg: /mymodule/core/xxxxx) (0=disable, 1=enable)
