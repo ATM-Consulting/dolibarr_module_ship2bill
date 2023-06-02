fbaf7cec1f9676ce33c02789b6d157a9c3ad598b
Florian Mortgat <50440633+atm-florianm@users.noreply.github.com>
2023-06-02 11:16:36 +0200

    Update core/triggers/interface_99_modShip2bill_Ship2billWorkflow.class.php
    Co-authored-by: John BOTELLA <john.botella@atm-consulting.fr>

--- core/triggers/interface_99_modShip2bill_Ship2billWorkflow.class.php
+++ core/triggers/interface_99_modShip2bill_Ship2billWorkflow.class.php
@@ -111,6 +111,36 @@ class InterfaceShip2billWorkflow
      * 	@param		conf		$conf		Object conf
      * 	@return		int						<0 if KO, 0 if no triggered ran, >0 if OK
      */
+    /**
+     * Function called when a Dolibarrr business event is done.
+     * All functions "run_trigger" are triggered if file
+     * is inside directory core/triggers
+     * For old versions compatibility
+     *
+     * 	@param		string		$action		Event action code
+     * 	@param		Object		$object		Object
+     * 	@param		User		$user		Object user
+     * 	@param		Translate	$langs		Object langs
+     * 	@param		conf		$conf		Object conf
+     * 	@return		int						<0 if KO, 0 if no triggered ran, >0 if OK
+     */
+    public function run_trigger($action, $object, $user, $langs, $conf){
+        return $this->runTrigger($action, $object, $user, $langs, $conf)
+    }
+    
+    
+    /**
+     * Function called when a Dolibarrr business event is done.
+     * All functions "run_trigger" are triggered if file
+     * is inside directory core/triggers
+     *
+     * 	@param		string		$action		Event action code
+     * 	@param		Object		$object		Object
+     * 	@param		User		$user		Object user
+     * 	@param		Translate	$langs		Object langs
+     * 	@param		conf		$conf		Object conf
+     * 	@return		int						<0 if KO, 0 if no triggered ran, >0 if OK
+     */
     public function runTrigger($action, $object, $user, $langs, $conf)
     {
         // Put here code you want to execute when a Dolibarr business events occurs.
